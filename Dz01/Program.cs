e1e172a4381f8ea926267175bec3e4e910a12490
Ivan <roman_ov18@mail.ru>
2022-08-10 21:57:44 +0400

    Первая и вторая задачи

---
+++ Dz01/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число");
+Console.Write("A:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+Console.Write("B:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int result = 1; 
+if(a > 0 && b > 0 )
+{
+  while(b > i)
+  {
+  result = a * result;
+  i=i+1;
+  // Console.WriteLine($"{result}");
+  }
+Console.WriteLine($"{result}");
+}   
+ 
+else Console.WriteLine("Введены некорректные данные");
+
+
+
+
+
---
+++ Dz02/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число  ");
+int i = Convert.ToInt32(Console.ReadLine());
+int temp = 0;
+
+while (i != 0)
+{
+    temp=temp + i %10;
+    i= i / 10;
+}
+
+Console.WriteLine($"{temp}");
+
